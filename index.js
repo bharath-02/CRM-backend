373d6a936a9895d063fb055f5d6f040d3fc991fd
Bharath02 <bharath.janakiram@banfico.com>
2021-08-05 11:59:24 +0530

    initial server setup

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require ('express');
+import express from "express";
 
-const app = express ();
+const app = express();
+const PORT = 4000;
 
-app.get ('/', (req, res) => {
-    res.json({data: 'data'})
-})
+app.get("/", (req, res) => {
+  res.send(`Node & Express server running on port ${PORT}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
